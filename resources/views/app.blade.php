4f2bc06b43fabbbac7480bf4dbb1acfa2b82b24b
IT.FAJAR <it.fajar@borwita.co.id>
2024-09-10 16:29:08 +0700

    Menambahkan layout halaman dashboard disamakan dengan halaman depan book finder

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,8 @@
         <title>Laravel</title>
 
         @vite('resources/js/app.js')
+        @vite('resources/css/app.css')
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
         @inertiaHead
     </head>
     <body>
