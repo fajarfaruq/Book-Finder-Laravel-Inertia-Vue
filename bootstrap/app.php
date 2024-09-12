b9d274cfb56b2b5177e70cb56f26d90072280d5c
IT.FAJAR <it.fajar@borwita.co.id>
2024-09-12 17:12:57 +0700

    Penambahan middleware login verification untuk mengecek user telah login atau belum

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,26 +2,25 @@
 
 // Add this line at the top
 use App\Http\Middleware\HandleInertiaRequests;
+use App\Http\Middleware\LoginVerification;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias(['LoginVerification' => LoginVerification::class,]);
     })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })
+    ->withExceptions(function (Exceptions $exceptions) {})
     // Find the line starting with ->withMiddleware and modify the contents
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
-            HandleInertiaRequests::class,
+            HandleInertiaRequests::class
         ]);
     })
     ->create();
